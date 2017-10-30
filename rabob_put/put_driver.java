38788facd89d6c31ac918d25315d3940a072aa09
HJRaBob <30286304+HJRaBob@users.noreply.github.com>
2017-10-30 20:50:58 +0900

    Add files via upload

---
+++ rabob_put/put_driver.java
@@ -0,0 +1,29 @@
+package rabob_put;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.util.Progressable;
+
+import java.io.*;
+import java.net.URI;
+
+public class put_driver {
+    public static void main(String[] args) throws IOException {
+        String localSrc = args[0];
+        String dst = args[1];
+
+        InputStream in =new BufferedInputStream(new FileInputStream(localSrc));
+
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(dst),conf);
+        OutputStream out = fs.create(new Path(dst), new Progressable() {
+            @Override
+            public void progress() {
+                System.out.print(".");
+            }
+        });
+        IOUtils.copyBytes(in,out,4096,true);
+    }
+}
